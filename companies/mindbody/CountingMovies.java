87624e5bfe83e191bc327985ce95678e304f72ea
axell-brendow <breno.axel@gmail.com>
2024-11-13 01:11:09 -0300

    Fix CountingMovies.java

--- companies/mindbody/CountingMovies.java
+++ companies/mindbody/CountingMovies.java
@@ -1,28 +1,27 @@
 package mindbody;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
+import java.net.*;
+import java.net.http.*;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.regex.Pattern;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class CountingMovies {
   private static CompletableFuture<Integer> countMovies(String title) {
-    final var url = String.format("https://jsonmock.hackerrank.com/api/moviesdata/search/?Title=%s", title);
-
     final var client = HttpClient.newHttpClient();
 
+    final var url = "https://jsonmock.hackerrank.com/api/moviesdata/search/?Title=" + title;
     final var request = HttpRequest.newBuilder().uri(URI.create(url)).build();
 
     return client.sendAsync(request, BodyHandlers.ofString())
       .thenApply(res -> {
-        final var json = res.body();
-        System.out.println(json);
-        final int total = Integer.parseInt(Pattern.compile("\"total\": (\\d+),").matcher(json).group(1));
-        return total;
+        String json = res.body();
+        Matcher matcher = Pattern.compile(".*\"total\":(\\d+),.*").matcher(json);
+        if (!matcher.matches()) throw new RuntimeException(
+          String.format("total field not found, title = %s, response = %s", title, json)
+        );
+        return Integer.parseInt(matcher.group(1));
       });
   }
 
@@ -30,7 +29,7 @@ public class CountingMovies {
     final var titles = new String[]{"star", "ab"};
     final var totals = new int[]{1, 15};
 
-    final List<CompletableFuture<Void>> futures = new ArrayList<>();
+    final Collection<CompletableFuture<Void>> futures = new ArrayList<>();
 
     for (int i = 0; i < titles.length; i++) {
       final var title = titles[i];
@@ -44,6 +43,6 @@ public class CountingMovies {
       futures.add(future);
     }
 
-    for (CompletableFuture<Void> completableFuture : futures) completableFuture.join();
+    futures.forEach(CompletableFuture::join);
   }
 }
